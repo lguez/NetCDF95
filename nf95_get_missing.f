0e9b3a41b63c25ef12ff89f81c00e71beefccc47
Lionel GUEZ <guez@lmd.ens.fr>
2020-01-02 16:03:26 +0100

    Add argument `new_missing` to `nf95_get_var`
    Overload generic interface `nf95_get_missing` with specific procedure
    `nf95_get_missing_int`.
    
    Add optional argument `new_missing` to generic interface `nf95_get_var`.

--- nf95_get_missing.f
+++ nf95_get_missing.f
@@ -1,12 +1,13 @@
 module nf95_get_missing_m
 
-  use netcdf, only: nf90_noerr, NF90_FILL_REAL
+  use netcdf, only: nf90_noerr
   use nf95_get_att_m, only: nf95_get_att
 
   implicit none
 
   interface nf95_get_missing
-     module procedure nf95_get_missing_real, nf95_get_missing_dble
+     module procedure nf95_get_missing_real, nf95_get_missing_dble, &
+          nf95_get_missing_int
   end interface nf95_get_missing
 
   private
@@ -61,5 +62,30 @@ contains
     end if
 
   end subroutine nf95_get_missing_dble
+
+  !**************************************************************************
+
+  subroutine nf95_get_missing_int(ncid, varid, missing)
+
+    use netcdf, only: NF90_FILL_INT
+
+    integer, intent(in)::  ncid, varid
+    integer, intent(out):: missing ! missing or fill value
+
+    ! Local:
+    integer ncerr
+
+    !-------------------------------------------------------------------
+
+    call nf95_get_att(ncid, varid, name = "missing_value", values = missing, &
+         ncerr = ncerr)
+
+    if (ncerr /= nf90_noerr) then
+       call nf95_get_att(ncid, varid, name = "_FillValue", values = missing, &
+            ncerr = ncerr)
+       if (ncerr /= nf90_noerr) missing = NF90_FILL_INT
+    end if
+
+  end subroutine nf95_get_missing_int
 
 end module nf95_get_missing_m
--- nf95_get_var.f
+++ nf95_get_var.f
@@ -1,8 +1,10 @@
 module nf95_get_var_m
 
-  use netcdf, only: nf90_get_var
+  use netcdf, only: nf90_get_var, NF90_NOERR
+  
   use handle_err_m, only: handle_err
   use check_start_count_m, only: check_start_count
+  use nf95_get_missing_m, only: nf95_get_missing
 
   implicit none
 
@@ -24,17 +26,20 @@ module nf95_get_var_m
 
 contains
 
-  subroutine nf95_get_var_FourByteReal(ncid, varid, values, start, ncerr)
+  subroutine nf95_get_var_FourByteReal(ncid, varid, values, start, &
+       new_missing, ncerr)
 
     use typesizes, only: FourByteReal
 
     integer, intent(in) :: ncid, varid
     real(kind = FourByteReal), intent(out) :: values
     integer, dimension(:), optional, intent(in) :: start
+    real(kind = FourByteReal), optional, intent(in) :: new_missing
     integer, intent(out), optional:: ncerr
 
     ! Local:
     character(len=*), parameter:: procedure_name = "nf95_get_var_FourByteReal"
+    real(kind = FourByteReal) missing
 
     !-------------------
 
@@ -44,18 +49,21 @@ contains
 
   !***********************
 
-  subroutine nf95_get_var_FourByteInt(ncid, varid, values, start, ncerr)
+  subroutine nf95_get_var_FourByteInt(ncid, varid, values, start, &
+       new_missing, ncerr)
 
     use typesizes, only: FourByteInt
 
     integer, intent(in) :: ncid, varid
     integer(kind = FourByteInt), intent(out) :: values
     integer, dimension(:), optional, intent(in) :: start
+    integer(kind = FourByteInt), optional, intent(in) :: new_missing
     integer, intent(out), optional:: ncerr
 
     ! Local:
     character(len=*), parameter:: procedure_name = "nf95_get_var_FourByteInt"
-
+    integer(kind = FourByteInt) missing
+    
     !-------------------
 
     include "nf95_get_var_scalar.h"
@@ -65,19 +73,21 @@ contains
   !***********************
 
   subroutine nf95_get_var_1D_FourByteReal(ncid, varid, values, start, &
-       count_nc, stride, map, ncerr)
+       count_nc, stride, map, new_missing, ncerr)
 
     use typesizes, only: FourByteReal
 
     integer,                         intent(in) :: ncid, varid
     real(kind = FourByteReal), intent(out) :: values(:)
     integer, dimension(:), optional, intent(in):: start, count_nc, stride, map
+    real(kind = FourByteReal), optional, intent(in) :: new_missing
     integer, intent(out), optional:: ncerr
 
     ! Local:
     character(len=*), parameter:: procedure_name = &
          "nf95_get_var_1D_FourByteReal"
     integer, parameter:: rank_values = 1
+    real(kind = FourByteReal) missing
 
     !-------------------
 
@@ -88,19 +98,21 @@ contains
   !***********************
 
   subroutine nf95_get_var_1D_FourByteInt(ncid, varid, values, start, &
-       count_nc, stride, map, ncerr)
+       count_nc, stride, map, new_missing, ncerr)
 
     use typesizes, only: FourByteInt
 
     integer,                         intent(in) :: ncid, varid
     integer(kind = FourByteInt), intent(out) :: values(:)
     integer, dimension(:), optional, intent(in) :: start, count_nc, stride, map
+    integer(kind = FourByteInt), optional, intent(in) :: new_missing
     integer, intent(out), optional:: ncerr
 
     ! Local:
     character(len=*), parameter:: procedure_name = &
          "nf95_get_var_1D_FourByteInt"
     integer, parameter:: rank_values = 1
+    integer(kind = FourByteInt) missing
 
     !-------------------
 
@@ -111,19 +123,21 @@ contains
   !***********************
 
   subroutine nf95_get_var_1D_EightByteReal(ncid, varid, values, start, &
-       count_nc, stride, map, ncerr)
+       count_nc, stride, map, new_missing, ncerr)
 
     use typesizes, only: eightByteReal
 
     integer,                         intent(in) :: ncid, varid
-    real (kind = EightByteReal),     intent(out) :: values(:)
+    real(kind = EightByteReal),     intent(out) :: values(:)
     integer, dimension(:), optional, intent(in):: start, count_nc, stride, map
+    real(kind = EightByteReal), optional, intent(in) :: new_missing
     integer, intent(out), optional:: ncerr
 
     ! Local:
     character(len=*), parameter:: procedure_name = &
          "nf95_get_var_1D_EightByteReal"
     integer, parameter:: rank_values = 1
+    real(kind = EightByteReal) missing
 
     !-------------------
 
@@ -134,19 +148,21 @@ contains
   !***********************
 
   subroutine nf95_get_var_2D_FourByteReal(ncid, varid, values, start, &
-       count_nc, stride, map, ncerr)
+       count_nc, stride, map, new_missing, ncerr)
 
     use typesizes, only: FourByteReal
 
     integer,                         intent(in) :: ncid, varid
-    real (kind = FourByteReal), intent(out) :: values(:, :)
+    real(kind = FourByteReal), intent(out) :: values(:, :)
     integer, dimension(:), optional, intent(in) :: start, count_nc, stride, map
+    real(kind = FourByteReal), optional, intent(in) :: new_missing
     integer, intent(out), optional:: ncerr
 
     ! Local:
     character(len=*), parameter:: procedure_name = &
          "nf95_get_var_2D_FourByteReal"
     integer, parameter:: rank_values = 2
+    real(kind = FourByteReal) missing
 
     !-------------------
 
@@ -157,19 +173,21 @@ contains
   !***********************
 
   subroutine nf95_get_var_2D_EightByteReal(ncid, varid, values, start, &
-       count_nc, stride, map, ncerr)
+       count_nc, stride, map, new_missing, ncerr)
 
     use typesizes, only: EightByteReal
 
     integer,                         intent(in) :: ncid, varid
-    real (kind = EightByteReal), intent(out) :: values(:, :)
+    real(kind = EightByteReal), intent(out) :: values(:, :)
     integer, dimension(:), optional, intent(in) :: start, count_nc, stride, map
+    real(kind = EightByteReal), optional, intent(in) :: new_missing
     integer, intent(out), optional:: ncerr
 
     ! Local:
     character(len=*), parameter:: procedure_name = &
          "nf95_get_var_2D_EightByteReal"
     integer, parameter:: rank_values = 2
+    real(kind = EightByteReal) missing
 
     !-------------------
 
@@ -180,19 +198,21 @@ contains
   !***********************
 
   subroutine nf95_get_var_2D_FourByteInt(ncid, varid, values, start, &
-       count_nc, stride, map, ncerr)
+       count_nc, stride, map, new_missing, ncerr)
 
     use typesizes, only: FourByteInt
 
     integer, intent(in):: ncid, varid
     integer(kind = FourByteInt), intent(out):: values(:, :)
     integer, dimension(:), optional, intent(in):: start, count_nc, stride, map
+    integer(kind = FourByteInt), optional, intent(in) :: new_missing
     integer, intent(out), optional:: ncerr
 
     ! Local:
     character(len=*), parameter:: procedure_name = &
          "nf95_get_var_2D_FourByteInt"
     integer, parameter:: rank_values = 2
+    integer(kind = FourByteInt) missing
 
     !-------------------
 
@@ -203,19 +223,21 @@ contains
   !***********************
 
   subroutine nf95_get_var_3D_FourByteInt(ncid, varid, values, start, &
-       count_nc, stride, map, ncerr)
+       count_nc, stride, map, new_missing, ncerr)
 
     use typesizes, only: FourByteInt
 
     integer, intent(in):: ncid, varid
     integer(kind = FourByteInt), intent(out):: values(:, :, :)
     integer, dimension(:), optional, intent(in):: start, count_nc, stride, map
+    integer(kind = FourByteInt), optional, intent(in) :: new_missing
     integer, intent(out), optional:: ncerr
 
     ! Local:
     character(len=*), parameter:: procedure_name = &
          "nf95_get_var_3D_FourByteInt"
     integer, parameter:: rank_values = 3
+    integer(kind = FourByteInt) missing
 
     !-------------------
 
@@ -226,19 +248,21 @@ contains
   !***********************
 
   subroutine nf95_get_var_3D_FourByteReal(ncid, varid, values, start, &
-       count_nc, stride, map, ncerr)
+       count_nc, stride, map, new_missing, ncerr)
 
     use typesizes, only: FourByteReal
 
     integer,                         intent(in) :: ncid, varid
-    real (kind = FourByteReal), intent(out) :: values(:, :, :)
+    real(kind = FourByteReal), intent(out) :: values(:, :, :)
     integer, dimension(:), optional, intent(in) :: start, count_nc, stride, map
+    real(kind = FourByteReal), optional, intent(in) :: new_missing
     integer, intent(out), optional:: ncerr
 
     ! Local:
     character(len=*), parameter:: procedure_name = &
          "nf95_get_var_3D_FourByteReal"
     integer, parameter:: rank_values = 3
+    real(kind = FourByteReal) missing
 
     !-------------------
 
@@ -249,19 +273,21 @@ contains
   !***********************
 
   subroutine nf95_get_var_3D_EightByteReal(ncid, varid, values, start, &
-       count_nc, stride, map, ncerr)
+       count_nc, stride, map, new_missing, ncerr)
 
     use typesizes, only: eightByteReal
 
     integer,                         intent(in) :: ncid, varid
-    real (kind = EightByteReal),     intent(out) :: values(:, :, :)
+    real(kind = EightByteReal),     intent(out) :: values(:, :, :)
     integer, dimension(:), optional, intent(in) :: start, count_nc, stride, map
+    real(kind = EightByteReal), optional, intent(in) :: new_missing
     integer, intent(out), optional:: ncerr
 
     ! Local:
     character(len=*), parameter:: procedure_name = &
          "nf95_get_var_3D_EightByteReal"
     integer, parameter:: rank_values = 3
+    real(kind = EightByteReal) missing
 
     !-------------------
 
@@ -272,19 +298,21 @@ contains
   !***********************
 
   subroutine nf95_get_var_4D_FourByteReal(ncid, varid, values, start, &
-       count_nc, stride, map, ncerr)
+       count_nc, stride, map, new_missing, ncerr)
 
     use typesizes, only: FourByteReal
 
     integer,                         intent(in) :: ncid, varid
-    real (kind = FourByteReal), intent(out) :: values(:, :, :, :)
+    real(kind = FourByteReal), intent(out) :: values(:, :, :, :)
     integer, dimension(:), optional, intent(in) :: start, count_nc, stride, map
+    real(kind = FourByteReal), optional, intent(in) :: new_missing
     integer, intent(out), optional:: ncerr
 
     ! Local:
     character(len=*), parameter:: procedure_name = &
          "nf95_get_var_4D_FourByteReal"
     integer, parameter:: rank_values = 4
+    real(kind = FourByteReal) missing
 
     !-------------------
 
@@ -295,19 +323,21 @@ contains
   !***********************
 
   subroutine nf95_get_var_4D_EightByteReal(ncid, varid, values, start, &
-       count_nc, stride, map, ncerr)
+       count_nc, stride, map, new_missing, ncerr)
 
     use typesizes, only: EightByteReal
 
     integer, intent(in):: ncid, varid
     real(kind = EightByteReal), intent(out):: values(:, :, :, :)
     integer, dimension(:), optional, intent(in):: start, count_nc, stride, map
+    real(kind = EightByteReal), optional, intent(in) :: new_missing
     integer, intent(out), optional:: ncerr
 
     ! Local:
     character(len=*), parameter:: procedure_name = &
          "nf95_get_var_4D_EightByteReal"
     integer, parameter:: rank_values = 4
+    real(kind = EightByteReal) missing
 
     !-------------------
 
@@ -318,19 +348,21 @@ contains
   !***********************
 
   subroutine nf95_get_var_5D_FourByteReal(ncid, varid, values, start, &
-       count_nc, stride, map, ncerr)
+       count_nc, stride, map, new_missing, ncerr)
 
     use typesizes, only: FourByteReal
 
     integer, intent(in):: ncid, varid
     real(kind = FourByteReal), intent(out):: values(:, :, :, :, :)
     integer, dimension(:), optional, intent(in):: start, count_nc, stride, map
+    real(kind = FourByteReal), optional, intent(in) :: new_missing
     integer, intent(out), optional:: ncerr
 
     ! Local:
     character(len=*), parameter:: procedure_name = &
          "nf95_get_var_5D_FourByteReal"
     integer, parameter:: rank_values = 5
+    real(kind = FourByteReal) missing
 
     !-------------------
 
@@ -341,19 +373,21 @@ contains
   !***********************
 
   subroutine nf95_get_var_5D_EightByteReal(ncid, varid, values, start, &
-       count_nc, stride, map, ncerr)
+       count_nc, stride, map, new_missing, ncerr)
 
     use typesizes, only: EightByteReal
 
     integer, intent(in):: ncid, varid
     real(kind = EightByteReal), intent(out):: values(:, :, :, :, :)
     integer, dimension(:), optional, intent(in):: start, count_nc, stride, map
+    real(kind = EightByteReal), optional, intent(in) :: new_missing
     integer, intent(out), optional:: ncerr
 
     ! Local:
     character(len=*), parameter:: procedure_name = &
          "nf95_get_var_5D_EightByteReal"
     integer, parameter:: rank_values = 5
+    real(kind = EightByteReal) missing
 
     !-------------------
 
