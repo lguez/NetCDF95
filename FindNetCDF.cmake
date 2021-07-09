f0538d197a556747417b22ae1c84fb27b7a4147c
Lionel GUEZ <guez@lmd.ens.fr>
2021-07-10 00:07:11 +0200

    Update
    Update to commit b8c3197 from cmake repository.

--- FindNetCDF.cmake
+++ FindNetCDF.cmake
@@ -7,18 +7,21 @@
 #[==[
 Provides the following variables:
 
-  * `NetCDF_FOUND`: Whether NetCDF was found or not.
-  * `NetCDF_INCLUDE_DIRS`: Include directories necessary to use NetCDF.
-  * `NetCDF_LIBRARIES`: Libraries necessary to use NetCDF.
-  * `NetCDF_VERSION`: The version of NetCDF found.
-  * `NetCDF::NetCDF`: A target to use with `target_link_libraries`.
-  * `NetCDF_HAS_PARALLEL`: Whether or not NetCDF was found with parallel IO support.
+* `NetCDF_FOUND`: Whether NetCDF was found or not.
+* `NetCDF_INCLUDE_DIRS`: Include directories necessary to use NetCDF.
+* `NetCDF_LIBRARIES`: Libraries necessary to use NetCDF.
+* `NetCDF_VERSION`: The version of NetCDF found.
+* `NetCDF::NetCDF`: A target to use with `target_link_libraries`.
+* `NetCDF_HAS_PARALLEL`: Whether or not NetCDF was found with parallel IO support.
 #]==]
+
+list(APPEND CMAKE_MESSAGE_CONTEXT FindNetCDF)
 
 function(FindNetCDF_get_is_parallel_aware include_dir)
   file(STRINGS "${include_dir}/netcdf_meta.h" _netcdf_lines
     REGEX "#define[ \t]+NC_HAS_PARALLEL[ \t]")
-  string(REGEX REPLACE ".*NC_HAS_PARALLEL[ \t]*([0-1]+).*" "\\1" _netcdf_has_parallel "${_netcdf_lines}")
+  string(REGEX REPLACE ".*NC_HAS_PARALLEL[ \t]*([0-1]+).*" "\\1"
+    _netcdf_has_parallel "${_netcdf_lines}")
   if (_netcdf_has_parallel)
     set(NetCDF_HAS_PARALLEL TRUE PARENT_SCOPE)
   else()
@@ -28,6 +31,7 @@ endfunction()
 
 # Try to find a CMake-built NetCDF.
 find_package(netCDF CONFIG QUIET)
+
 if (netCDF_FOUND)
   # Forward the variables in a consistent way.
   set(NetCDF_FOUND "${netCDF_FOUND}")
@@ -56,89 +60,94 @@ if (netCDF_FOUND)
   endif ()
 
   FindNetCDF_get_is_parallel_aware("${NetCDF_INCLUDE_DIRS}")
-  # Skip the rest of the logic in this file.
-  return ()
-endif ()
+else()
+  find_package(PkgConfig QUIET)
 
-find_package(PkgConfig QUIET)
-if (PkgConfig_FOUND)
-  pkg_check_modules(_NetCDF QUIET netcdf IMPORTED_TARGET)
-  if (_NetCDF_FOUND)
-    unset(netCDF_DIR CACHE)
-    pkg_get_variable(netcdf_pcfiledir netcdf pcfiledir)
-    message(STATUS "${CMAKE_CURRENT_LIST_FILE}")
-    message(STATUS "-- Location of .pc file: ${netcdf_pcfiledir}")
-    message(STATUS "-- NetCDF include directories: ${_NetCDF_INCLUDE_DIRS}")
-    message(STATUS "-- NetCDF libraries: ${_NetCDF_LINK_LIBRARIES}")
-    # Forward the variables in a consistent way.
-    set(NetCDF_FOUND "${_NetCDF_FOUND}")
-    set(NetCDF_INCLUDE_DIRS "${_NetCDF_INCLUDE_DIRS}")
-    set(NetCDF_LIBRARIES "${_NetCDF_LIBRARIES}")
-    set(NetCDF_VERSION "${_NetCDF_VERSION}")
+  if (PkgConfig_FOUND)
+    pkg_check_modules(_NetCDF QUIET netcdf IMPORTED_TARGET)
+    if (_NetCDF_FOUND)
+      unset(netCDF_DIR CACHE)
+      pkg_get_variable(netcdf_pcfiledir netcdf pcfiledir)
+      message(VERBOSE "Location of .pc file: ${netcdf_pcfiledir}")
+      message(VERBOSE "NetCDF include directories: ${_NetCDF_INCLUDE_DIRS}")
+      message(VERBOSE "NetCDF libraries: ${_NetCDF_LINK_LIBRARIES}")
+      # Forward the variables in a consistent way.
+      set(NetCDF_FOUND "${_NetCDF_FOUND}")
+      set(NetCDF_INCLUDE_DIRS "${_NetCDF_INCLUDE_DIRS}")
+      set(NetCDF_LIBRARIES "${_NetCDF_LIBRARIES}")
+      set(NetCDF_VERSION "${_NetCDF_VERSION}")
+
+      include(FindPackageHandleStandardArgs)
+      find_package_handle_standard_args(NetCDF
+	REQUIRED_VARS NetCDF_LIBRARIES
+	# This is not required because system-default include paths
+	# are not reported by `FindPkgConfig`, so this might be
+	# empty. Assume that if we have a library, the include
+	# directories are fine (if any) since PkgConfig reported that
+	# the package was found.  NetCDF_INCLUDE_DIRS
+	VERSION_VAR NetCDF_VERSION)
+
+      if (NOT TARGET NetCDF::NetCDF)
+	add_library(NetCDF::NetCDF INTERFACE IMPORTED)
+	set_target_properties(NetCDF::NetCDF PROPERTIES
+          INTERFACE_LINK_LIBRARIES "PkgConfig::_NetCDF")
+      endif ()
+
+      FindNetCDF_get_is_parallel_aware("${_NetCDF_INCLUDEDIR}")
+    endif ()
+  endif ()
+
+  if (NOT PkgConfig_FOUND OR NOT _NetCDF_FOUND)
+    find_path(NetCDF_INCLUDE_DIR
+      NAMES netcdf.h
+      DOC "netcdf include directories")
+    ##mark_as_advanced(NetCDF_INCLUDE_DIR)
+
+    find_library(NetCDF_LIBRARY
+      NAMES netcdf
+      DOC "netcdf library")
+    ##mark_as_advanced(NetCDF_LIBRARY)
+
+    if (NetCDF_INCLUDE_DIR)
+      unset(netCDF_DIR CACHE)
+      file(STRINGS "${NetCDF_INCLUDE_DIR}/netcdf_meta.h" _netcdf_version_lines
+	REGEX "#define[ \t]+NC_VERSION_(MAJOR|MINOR|PATCH|NOTE)")
+      string(REGEX REPLACE ".*NC_VERSION_MAJOR *\([0-9]*\).*" "\\1"
+	_netcdf_version_major "${_netcdf_version_lines}")
+      string(REGEX REPLACE ".*NC_VERSION_MINOR *\([0-9]*\).*" "\\1"
+	_netcdf_version_minor "${_netcdf_version_lines}")
+      string(REGEX REPLACE ".*NC_VERSION_PATCH *\([0-9]*\).*" "\\1"
+	_netcdf_version_patch "${_netcdf_version_lines}")
+      string(REGEX REPLACE ".*NC_VERSION_NOTE *\"\([^\"]*\)\".*" "\\1"
+	_netcdf_version_note "${_netcdf_version_lines}")
+      set(NetCDF_VERSION
+	"${_netcdf_version_major}.${_netcdf_version_minor}.${_netcdf_version_patch}${_netcdf_version_note}")
+      unset(_netcdf_version_major)
+      unset(_netcdf_version_minor)
+      unset(_netcdf_version_patch)
+      unset(_netcdf_version_note)
+      unset(_netcdf_version_lines)
+
+      FindNetCDF_get_is_parallel_aware("${NetCDF_INCLUDE_DIR}")
+    endif ()
 
     include(FindPackageHandleStandardArgs)
     find_package_handle_standard_args(NetCDF
-      REQUIRED_VARS NetCDF_LIBRARIES
-      # This is not required because system-default include paths are not
-      # reported by `FindPkgConfig`, so this might be empty. Assume that if we
-      # have a library, the include directories are fine (if any) since
-      # PkgConfig reported that the package was found.
-      # NetCDF_INCLUDE_DIRS
+      REQUIRED_VARS NetCDF_LIBRARY NetCDF_INCLUDE_DIR
       VERSION_VAR NetCDF_VERSION)
 
-    if (NOT TARGET NetCDF::NetCDF)
-      add_library(NetCDF::NetCDF INTERFACE IMPORTED)
-      set_target_properties(NetCDF::NetCDF PROPERTIES
-        INTERFACE_LINK_LIBRARIES "PkgConfig::_NetCDF")
+    if (NetCDF_FOUND)
+      set(NetCDF_INCLUDE_DIRS "${NetCDF_INCLUDE_DIR}")
+      set(NetCDF_LIBRARIES "${NetCDF_LIBRARY}")
+
+      if (NOT TARGET NetCDF::NetCDF)
+	add_library(NetCDF::NetCDF UNKNOWN IMPORTED)
+	set_target_properties(NetCDF::NetCDF PROPERTIES
+	  IMPORTED_LOCATION "${NetCDF_LIBRARY}"
+	  INTERFACE_INCLUDE_DIRECTORIES "${NetCDF_INCLUDE_DIR}")
+      endif ()
     endif ()
-
-    FindNetCDF_get_is_parallel_aware("${_NetCDF_INCLUDEDIR}")
-    # Skip the rest of the logic in this file.
-    return ()
-  endif ()
+  endif()
 endif ()
 
-find_path(NetCDF_INCLUDE_DIR
-  NAMES netcdf.h
-  DOC "netcdf include directories")
-##mark_as_advanced(NetCDF_INCLUDE_DIR)
-
-find_library(NetCDF_LIBRARY
-  NAMES netcdf
-  DOC "netcdf library")
-##mark_as_advanced(NetCDF_LIBRARY)
-
-if (NetCDF_INCLUDE_DIR)
-  unset(netCDF_DIR CACHE)
-  file(STRINGS "${NetCDF_INCLUDE_DIR}/netcdf_meta.h" _netcdf_version_lines
-    REGEX "#define[ \t]+NC_VERSION_(MAJOR|MINOR|PATCH|NOTE)")
-  string(REGEX REPLACE ".*NC_VERSION_MAJOR *\([0-9]*\).*" "\\1" _netcdf_version_major "${_netcdf_version_lines}")
-  string(REGEX REPLACE ".*NC_VERSION_MINOR *\([0-9]*\).*" "\\1" _netcdf_version_minor "${_netcdf_version_lines}")
-  string(REGEX REPLACE ".*NC_VERSION_PATCH *\([0-9]*\).*" "\\1" _netcdf_version_patch "${_netcdf_version_lines}")
-  string(REGEX REPLACE ".*NC_VERSION_NOTE *\"\([^\"]*\)\".*" "\\1" _netcdf_version_note "${_netcdf_version_lines}")
-  set(NetCDF_VERSION "${_netcdf_version_major}.${_netcdf_version_minor}.${_netcdf_version_patch}${_netcdf_version_note}")
-  unset(_netcdf_version_major)
-  unset(_netcdf_version_minor)
-  unset(_netcdf_version_patch)
-  unset(_netcdf_version_note)
-  unset(_netcdf_version_lines)
-
-  FindNetCDF_get_is_parallel_aware("${NetCDF_INCLUDE_DIR}")
-endif ()
-
-include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(NetCDF
-  REQUIRED_VARS NetCDF_LIBRARY NetCDF_INCLUDE_DIR
-  VERSION_VAR NetCDF_VERSION)
-
-if (NetCDF_FOUND)
-  set(NetCDF_INCLUDE_DIRS "${NetCDF_INCLUDE_DIR}")
-  set(NetCDF_LIBRARIES "${NetCDF_LIBRARY}")
-
-  if (NOT TARGET NetCDF::NetCDF)
-    add_library(NetCDF::NetCDF UNKNOWN IMPORTED)
-    set_target_properties(NetCDF::NetCDF PROPERTIES
-      IMPORTED_LOCATION "${NetCDF_LIBRARY}"
-      INTERFACE_INCLUDE_DIRECTORIES "${NetCDF_INCLUDE_DIR}")
-  endif ()
-endif ()
+list(POP_BACK CMAKE_MESSAGE_CONTEXT)
--- FindNetCDF_Fortran.cmake
+++ FindNetCDF_Fortran.cmake
@@ -1,33 +1,37 @@
-find_package(PkgConfig REQUIRED)
-pkg_check_modules(NetCDF_Fortran REQUIRED IMPORTED_TARGET netcdf-fortran)
-pkg_get_variable(netcdf_fortran_pcfiledir netcdf-fortran pcfiledir)
-message(STATUS "${CMAKE_CURRENT_LIST_FILE}")
-message(STATUS "-- Location of .pc file: ${netcdf_fortran_pcfiledir}")
+list(APPEND CMAKE_MESSAGE_CONTEXT FindNetCDF_Fortran)
 
-message(STATUS
-  "-- NetCDF-Fortran include directories: ${NetCDF_Fortran_INCLUDE_DIRS}")
+if (NOT TARGET NetCDF_Fortran::NetCDF_Fortran)
+  find_package(PkgConfig REQUIRED)
 
-message(STATUS "-- NetCDF-Fortran libraries: ${NetCDF_Fortran_LINK_LIBRARIES}")
+  pkg_check_modules(NetCDF_Fortran REQUIRED IMPORTED_TARGET GLOBAL
+    netcdf-fortran)
 
-if(PKG_CONFIG_VERSION_STRING VERSION_LESS "0.29.2")
-  # pkg-config strips system flags out of cflags. They do not appear
-  # with prefix -isystem when pkg-config is run with
-  # --cflags-only-I. So cmake does not get them. So we have to
-  # duplicate the call to pkg_get_variable that is already in
-  # pkg_check_modules.
-  pkg_get_variable(pkg_netcdf_fortran_includedir netcdf-fortran includedir)
+  pkg_get_variable(netcdf_fortran_pcfiledir netcdf-fortran pcfiledir)
+  message(VERBOSE "Location of .pc file: ${netcdf_fortran_pcfiledir}")
 
-  target_include_directories(PkgConfig::NetCDF_Fortran INTERFACE
-    ${pkg_netcdf_fortran_includedir})
+  message(VERBOSE
+    "NetCDF-Fortran include directories: ${NetCDF_Fortran_INCLUDE_DIRS}")
 
-  message(STATUS
-    "-- pkg_netcdf_fortran_includedir: ${pkg_netcdf_fortran_includedir}")
+  message(VERBOSE "NetCDF-Fortran libraries: ${NetCDF_Fortran_LINK_LIBRARIES}")
+
+  if(PKG_CONFIG_VERSION_STRING VERSION_LESS "0.29.2"
+      OR PKG_CONFIG_VERSION_STRING VERSION_GREATER "1.1")
+    # pkg-config strips system flags out of cflags. They do not appear
+    # with prefix -isystem when pkg-config is run with
+    # --cflags-only-I. So cmake does not get them. So we have to
+    # duplicate the call to pkg_get_variable that is already in
+    # pkg_check_modules.
+    # Version > 1.1 means we are using pkgconf from pkgconf.org
+    pkg_get_variable(pkg_netcdf_fortran_includedir netcdf-fortran includedir)
+
+    target_include_directories(PkgConfig::NetCDF_Fortran INTERFACE
+      ${pkg_netcdf_fortran_includedir})
+
+    message(VERBOSE
+      "pkg_netcdf_fortran_includedir: ${pkg_netcdf_fortran_includedir}")
+  endif()
+
+  add_library(NetCDF_Fortran::NetCDF_Fortran ALIAS PkgConfig::NetCDF_Fortran)
 endif()
 
-add_library(NetCDF_Fortran INTERFACE IMPORTED)
-
-set_target_properties(NetCDF_Fortran PROPERTIES
-  INTERFACE_LINK_LIBRARIES PkgConfig::NetCDF_Fortran)
-
-target_link_libraries(NetCDF_Fortran INTERFACE NetCDF::NetCDF)
-add_library(NetCDF_Fortran::NetCDF_Fortran ALIAS NetCDF_Fortran)
+list(POP_BACK CMAKE_MESSAGE_CONTEXT)
