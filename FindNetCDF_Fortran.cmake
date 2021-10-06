5bc7b6521ea7e4f11642c368fa0e0bc79e87e49b
Lionel GUEZ <guez@lmd.ens.fr>
2021-10-06 23:56:31 +0200

    Update to commit 1270b6a from cmake repository

--- FindNetCDF_Fortran.cmake
+++ FindNetCDF_Fortran.cmake
@@ -1,18 +1,18 @@
 list(APPEND CMAKE_MESSAGE_CONTEXT FindNetCDF_Fortran)
 
-if (NOT TARGET NetCDF_Fortran::NetCDF_Fortran)
+if (NOT TARGET NetCDF_Fortran::netcdff)
   find_package(PkgConfig REQUIRED)
 
-  pkg_check_modules(NetCDF_Fortran REQUIRED IMPORTED_TARGET GLOBAL
+  pkg_check_modules(netcdff REQUIRED IMPORTED_TARGET GLOBAL
     netcdf-fortran)
 
   pkg_get_variable(netcdf_fortran_pcfiledir netcdf-fortran pcfiledir)
   message(VERBOSE "Location of .pc file: ${netcdf_fortran_pcfiledir}")
 
   message(VERBOSE
-    "NetCDF-Fortran include directories: ${NetCDF_Fortran_INCLUDE_DIRS}")
+    "NetCDF-Fortran include directories: ${netcdff_INCLUDE_DIRS}")
 
-  message(VERBOSE "NetCDF-Fortran libraries: ${NetCDF_Fortran_LINK_LIBRARIES}")
+  message(VERBOSE "NetCDF-Fortran libraries: ${netcdff_LINK_LIBRARIES}")
 
   if(PKG_CONFIG_VERSION_STRING VERSION_LESS "0.29.2"
       OR PKG_CONFIG_VERSION_STRING VERSION_GREATER "1.1")
@@ -24,15 +24,15 @@ if (NOT TARGET NetCDF_Fortran::NetCDF_Fortran)
     # Version > 1.1 means we are using pkgconf from pkgconf.org
     pkg_get_variable(pkg_netcdf_fortran_includedir netcdf-fortran includedir)
 
-    target_include_directories(PkgConfig::NetCDF_Fortran INTERFACE
+    target_include_directories(PkgConfig::netcdff INTERFACE
       ${pkg_netcdf_fortran_includedir})
 
     message(VERBOSE
       "pkg_netcdf_fortran_includedir: ${pkg_netcdf_fortran_includedir}")
   endif()
 
-  target_link_libraries(PkgConfig::NetCDF_Fortran INTERFACE NetCDF::NetCDF)
-  add_library(NetCDF_Fortran::NetCDF_Fortran ALIAS PkgConfig::NetCDF_Fortran)
+  target_link_libraries(PkgConfig::netcdff INTERFACE NetCDF::NetCDF)
+  add_library(NetCDF_Fortran::netcdff ALIAS PkgConfig::netcdff)
 endif()
 
 list(POP_BACK CMAKE_MESSAGE_CONTEXT)
