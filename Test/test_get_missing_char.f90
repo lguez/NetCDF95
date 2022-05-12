076ed00d9962fe358b1a56e82770bc0d1c183fc6
Lionel GUEZ <guez@lmd.ens.fr>
2022-05-12 18:59:12 +0200

    Add program `test_get_missing_char`
    With the corresponding NetCDF test file.

---
+++ Test/test_get_missing_char.f90
@@ -0,0 +1,37 @@
+program test_get_missing_char
+
+  use netcdf, only: nf90_nowrite, nf90_global
+  use netcdf95, only: nf95_open, nf95_close, nf95_get_missing, nf95_inq_varid
+
+  implicit none
+
+  integer ncid, varid
+  character missing
+
+  !-------------------------------------------------------------------
+
+  call nf95_open("test_get_missing_char.nc", nf90_nowrite, ncid)
+
+  ! Global:
+  call nf95_get_missing(ncid, nf90_global, missing)
+  print *, 'missing nf90_global = "', missing, '"'
+  print *, "iachar(missing) = ", iachar(missing)
+
+  ! x:
+  call nf95_inq_varid(ncid, "x", varid)
+  call nf95_get_missing(ncid, varid, missing)
+  print *, 'missing x = "', missing, '"'
+
+  ! y:
+  call nf95_inq_varid(ncid, "y", varid)
+  call nf95_get_missing(ncid, varid, missing)
+  print *, 'missing y = "', missing, '"'
+  
+  ! z:
+  call nf95_inq_varid(ncid, "z", varid)
+  call nf95_get_missing(ncid, varid, missing)
+  print *, 'missing z = "', missing, '"'
+  
+  call nf95_close(ncid)
+
+end program test_get_missing_char
