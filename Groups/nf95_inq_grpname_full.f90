3b303767361d7a97ec4598450934c093640fc3fc
Lionel GUEZ <guez@lmd.ens.fr>
2022-06-25 00:56:56 +0200

    Use `NF90_INQ_GRPNAME_LEN`
    Use `NF90_INQ_GRPNAME_LEN` instead of `NF90_MAX_NAME`. As the name is
    an absolute path, I guess it can be larger than `NF90_MAX_NAME`.

--- Groups/nf95_inq_grpname_full.f90
+++ Groups/nf95_inq_grpname_full.f90
@@ -7,7 +7,7 @@ contains
   subroutine nf95_inq_grpname_full(grpid, name, ncerr)
 
     use handle_err_m, only: handle_err
-    use netcdf, only: nf90_inq_grpname_full, NF90_MAX_NAME, nf90_noerr
+    use netcdf, only: nf90_inq_grpname_full, NF90_INQ_GRPNAME_LEN
 
     integer, intent(in):: grpid
     character(len = :), allocatable, intent(out):: name ! absolute path
@@ -15,20 +15,19 @@ contains
 
     ! Local:
     integer ncerr_not_opt, nclen
-    character(len = NF90_MAX_NAME) name_local
 
     !-------------------------------------------------------------------
 
-    ncerr_not_opt = nf90_inq_grpname_full(grpid, nclen, name_local)
+    ncerr_not_opt = NF90_INQ_GRPNAME_LEN(grpid, nclen)
+    call handle_err("nf95_inq_grpname_full -- NF90_INQ_GRPNAME_LEN", &
+         ncerr_not_opt, grpid)
+    allocate(character(len = nclen):: name)
+    ncerr_not_opt = nf90_inq_grpname_full(grpid, nclen, name)
 
     if (present(ncerr)) then
        ncerr = ncerr_not_opt
     else
        call handle_err("nf95_inq_grpname_full", ncerr_not_opt, grpid)
-    end if
-
-    if (ncerr_not_opt == nf90_noerr) then
-       name = name_local(:nclen)
     end if
 
   end subroutine nf95_inq_grpname_full
