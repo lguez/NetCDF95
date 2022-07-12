843b1bb41f5a09b142b692e72f69c7756da8c11f
Lionel GUEZ <guez@lmd.ens.fr>
2022-07-12 20:22:36 +0200

    Use `nf95_noerr` rather than `nf90_noerr`
    See commit 904ac06b.

--- Tests/test_put_var_char.f90
+++ Tests/test_put_var_char.f90
@@ -1,8 +1,8 @@
 program test_put_var_char
   
-  use netcdf, only: nf90_noclobber, nf90_put_var, nf90_char, nf90_noerr
+  use netcdf, only: nf90_noclobber, nf90_put_var, nf90_char
   use netcdf95, only: nf95_abort, nf95_close, nf95_create, nf95_def_dim, &
-       nf95_def_var, nf95_enddef
+       nf95_def_var, nf95_enddef, nf95_noerr
 
   implicit none
 
@@ -22,7 +22,7 @@ program test_put_var_char
   ! Using trim removes trailing blanks, prefill provides null
   ! termination, so C programs can later get intended string.
   status = nf90_put_var(ncid, oceanid, trim(ocean))
-  if(status /= nf90_noerr) call nf95_abort("test_char", status)
+  if(status /= nf95_noerr) call nf95_abort("test_char", status)
   call nf95_close(ncid)
 
 end program test_put_var_char
