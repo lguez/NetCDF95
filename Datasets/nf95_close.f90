27972bdab1b3bbda8ac9332a4f0d2bf91cf55086
Lionel GUEZ <guez@lmd.ens.fr>
2022-07-12 18:52:05 +0200

    Polish

--- Datasets/nf95_close.f90
+++ Datasets/nf95_close.f90
@@ -6,13 +6,15 @@ contains
 
   subroutine nf95_close(ncid, ncerr)
 
+    ! Note that this procedure is called by nf95_abort, so it cannot
+    ! call it.
 
     use netcdf, only: nf90_close, nf90_strerror
 
     use nf95_constants, only: nf95_noerr
 
-    integer, intent( in) :: ncid
-    integer, intent(out), optional :: ncerr
+    integer, intent(in):: ncid
+    integer, intent(out), optional:: ncerr
 
     ! Variable local to the procedure:
     integer ncerr_not_opt
