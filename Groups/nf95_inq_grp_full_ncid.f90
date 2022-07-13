4651cf4599cc55059dc855299f889c993eaae443
Lionel GUEZ <guez@lmd.ens.fr>
2022-07-13 11:13:25 +0200

    Use directly the NetCDF C interface

--- Groups/nf95_inq_grp_full_ncid.f90
+++ Groups/nf95_inq_grp_full_ncid.f90
@@ -6,8 +6,10 @@ contains
 
   subroutine nf95_inq_grp_full_ncid(ncid, full_name, grpid, ncerr)
 
+    use, intrinsic:: ISO_C_BINDING
+
+    use nc_constants, only: NC_NOERR
     use nf95_abort_m, only: nf95_abort
-    use netcdf, only: nf90_inq_grp_full_ncid
     use nf95_constants, only: nf95_noerr
 
     integer, intent(in):: ncid ! can be the file id or a group id
@@ -21,20 +23,37 @@ contains
     integer, intent(out), optional:: ncerr
 
     ! Local:
-    integer ncerr_not_opt
+    Integer(C_INT) cncerr, cgrp_ncid
+
+    Interface
+       Integer(C_INT) Function nc_inq_grp_full_ncid(cncid, full_name, &
+            cgrp_ncid) BIND(C)
+         import c_int, C_CHAR
+         Integer(C_INT), VALUE, Intent(IN):: cncid
+         Character(KIND=C_CHAR), Intent(IN):: full_name(*)
+         Integer(C_INT), Intent(ouT) :: cgrp_ncid
+       End Function nc_inq_grp_full_ncid
+    end Interface
 
     !------------------------------------------------------------
 
-    ncerr_not_opt = nf90_inq_grp_full_ncid(ncid, full_name, grpid)
-    
-    if (present(ncerr)) then
-       ncerr = ncerr_not_opt
+    ! We assume that the C character kind is the same as the default
+    ! character kind:
+    cncerr = nc_inq_grp_full_ncid(int(ncid, c_int), full_name // c_null_char, &
+         cgrp_ncid)
+
+    if (cncerr == NC_NOERR) then
+       grpid = cgrp_ncid
+       if (present(ncerr)) ncerr = nf95_noerr
     else
-       if (ncerr_not_opt /= nf95_noerr) call &
-            nf95_abort("nf95_inq_grp_full_ncid " // trim(full_name), &
-            ncerr_not_opt, ncid)
+       if (present(ncerr)) then
+          ncerr = cncerr
+       else
+          call nf95_abort("nf95_inq_grp_full_ncid " // trim(full_name), &
+            int(cncerr), ncid)
+       end if
     end if
 
   end subroutine nf95_inq_grp_full_ncid
-    
+
 end module nf95_inq_grp_full_ncid_m
