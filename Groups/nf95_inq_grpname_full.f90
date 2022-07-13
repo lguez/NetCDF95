8532c0bc452510583a160057153fc7721a2b471e
Lionel GUEZ <guez@lmd.ens.fr>
2022-07-13 14:18:18 +0200

    Rename grpid and name to ncid and `full_name`
    Rename dummy arguments grpid and name to ncid and `full_name`. ncid
    and `full_name` are the names in the NetCDF C interface.

--- Groups/nf95_inq_grpname_full.f90
+++ Groups/nf95_inq_grpname_full.f90
@@ -4,14 +4,14 @@ module nf95_inq_grpname_full_m
 
 contains
 
-  subroutine nf95_inq_grpname_full(grpid, name, ncerr)
+  subroutine nf95_inq_grpname_full(ncid, full_name, ncerr)
 
     use nf95_abort_m, only: nf95_abort
     use netcdf, only: nf90_inq_grpname_full, NF90_INQ_GRPNAME_LEN
     use nf95_constants, only: nf95_noerr
 
-    integer, intent(in):: grpid
-    character(len = :), allocatable, intent(out):: name ! absolute path
+    integer, intent(in):: ncid
+    character(len = :), allocatable, intent(out):: full_name ! absolute path
     integer, intent(out), optional:: ncerr
 
     ! Local:
@@ -19,18 +19,18 @@ contains
 
     !-------------------------------------------------------------------
 
-    ncerr_not_opt = NF90_INQ_GRPNAME_LEN(grpid, nclen)
+    ncerr_not_opt = NF90_INQ_GRPNAME_LEN(ncid, nclen)
     if (ncerr_not_opt /= nf95_noerr) call &
          nf95_abort("nf95_inq_grpname_full -- NF90_INQ_GRPNAME_LEN", &
-         ncerr_not_opt, grpid)
-    allocate(character(len = nclen):: name)
-    ncerr_not_opt = nf90_inq_grpname_full(grpid, nclen, name)
+         ncerr_not_opt, ncid)
+    allocate(character(len = nclen):: full_name)
+    ncerr_not_opt = nf90_inq_grpname_full(ncid, nclen, full_name)
 
     if (present(ncerr)) then
        ncerr = ncerr_not_opt
     else
        if (ncerr_not_opt /= nf95_noerr) call &
-            nf95_abort("nf95_inq_grpname_full", ncerr_not_opt, grpid)
+            nf95_abort("nf95_inq_grpname_full", ncerr_not_opt, ncid)
     end if
 
   end subroutine nf95_inq_grpname_full
