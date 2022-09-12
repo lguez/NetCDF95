7bbbd10132398cf386442b6a6903dca30e9fdefc
Lionel GUEZ <guez@lmd.ens.fr>
2022-09-12 23:11:25 +0200

    Define constants
    The motivation is to no longer depend on NetCDF-Fortran. I think it
    would be very difficult to take the values of the constants at build
    time from the NetCDF C library. The NetCDF-Fortran library just
    duplicates the definitions, as we are doing here.

--- nf95_constants.f90
+++ nf95_constants.f90
@@ -6,6 +6,9 @@ module nf95_constants
 
   Integer, Parameter:: Nf95_NOERR = NC_NOERR
   Integer, Parameter:: Nf95_ENOGRP = NC_ENOGRP
+  integer, parameter:: nf95_nowrite = 0
+  real, parameter:: nf95_fill_real = 9.9692099683868690e+36
+  integer, parameter:: nf95_max_name = 256
 
   private NC_NOERR, NC_ENOGRP
 
