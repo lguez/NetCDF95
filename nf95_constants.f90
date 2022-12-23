30b7a039ab8b5bcd8a470b32c8862e1cd5f69401
Lionel GUEZ <guez@lmd.ens.fr>
2022-12-23 11:07:55 +0100

    Define additional constants

--- nf95_constants.f90
+++ nf95_constants.f90
@@ -6,7 +6,8 @@ module nf95_constants
 
   Integer, Parameter:: Nf95_NOERR = NC_NOERR
   Integer, Parameter:: Nf95_ENOGRP = NC_ENOGRP
-  integer, parameter:: nf95_nowrite = 0
+  integer, parameter:: nf95_nowrite = 0, nf95_clobber   = 0, nf95_global = 0
+  integer, parameter:: nf95_unlimited = 0
   real, parameter:: nf95_fill_real = 9.9692099683868690e+36
   integer, parameter:: nf95_max_name = 256, nf95_float = 5, nf95_double = 6
 
