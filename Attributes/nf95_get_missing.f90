2838fc0545179114d0896b4d7e80713e1aae3473
Lionel GUEZ <guez@lmd.ens.fr>
2023-06-16 16:50:13 +0200

    Overload with `nf95_get_missing_short_int`
    Overload `nf95_get_missing` with `nf95_get_missing_short_int`.

--- Attributes/nf95_get_missing.f90
+++ Attributes/nf95_get_missing.f90
@@ -7,7 +7,8 @@ module nf95_get_missing_m
 
   interface nf95_get_missing
      module procedure nf95_get_missing_real, nf95_get_missing_dble, &
-          nf95_get_missing_int, nf95_get_missing_char
+          nf95_get_missing_short_int, nf95_get_missing_int, &
+          nf95_get_missing_char
   end interface nf95_get_missing
 
   private
@@ -62,6 +63,32 @@ contains
     end if
 
   end subroutine nf95_get_missing_dble
+
+  !**************************************************************************
+
+  subroutine nf95_get_missing_short_int(ncid, varid, missing)
+
+    use netcdf, only: NF90_FILL_short
+    use typesizes, only: TwoByteInt
+
+    integer, intent(in)::  ncid, varid
+    integer(kind = TwoByteInt), intent(out):: missing ! missing or fill value
+
+    ! Local:
+    integer ncerr
+
+    !-------------------------------------------------------------------
+
+    call nf95_get_att(ncid, varid, name = "missing_value", values = missing, &
+         ncerr = ncerr)
+
+    if (ncerr /= nf90_noerr) then
+       call nf95_get_att(ncid, varid, name = "_FillValue", values = missing, &
+            ncerr = ncerr)
+       if (ncerr /= nf90_noerr) missing = NF90_FILL_short
+    end if
+
+  end subroutine nf95_get_missing_short_int
 
   !**************************************************************************
 
