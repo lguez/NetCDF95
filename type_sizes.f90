867012d02ed044d45cbca0fe09e2a144429c0290
Lionel GUEZ <guez@lmd.ens.fr>
2023-07-20 16:49:59 +0200

    Polish

--- type_sizes.f90
+++ type_sizes.f90
@@ -11,36 +11,39 @@ module type_sizes
 
   implicit none
 
-  integer, parameter ::   OneByteInt = selected_int_kind(2), &
-       TwoByteInt = selected_int_kind(4), &
-       FourByteInt = selected_int_kind(9), &
+  integer, parameter :: OneByteInt = selected_int_kind(2), &
+       TwoByteInt = selected_int_kind(4), FourByteInt = selected_int_kind(9), &
        EightByteInt = selected_int_kind(18)
-
-  integer, parameter ::                                          &
-       FourByteReal = selected_real_kind(P =  6, R =  37), &
+  integer, parameter :: FourByteReal = selected_real_kind(P = 6, R = 37), &
        EightByteReal = selected_real_kind(P = 13, R = 307)
-contains
-  logical function byteSizesOK()
-    ! Users may call this function once to ensure that the kind parameters
-    !   the module defines are available with the current compiler.
-    ! We can't ensure that the two REAL kinds are actually four and
-    !   eight bytes long, but we can ensure that they are distinct.
-    ! Early Fortran 90 compilers would sometimes report incorrect results for
-    !   the bit_size intrinsic, but I haven't seen this in a long time.
 
-    ! Local variables
-    integer (kind =  OneByteInt)  :: One
-    integer (kind =  TwoByteInt)  :: Two
-    integer (kind = FourByteInt)  :: Four
+contains
+
+  logical function byteSizesOK()
+
+    ! Users may call this function once to ensure that the kind
+    ! parameters the module defines are available with the current
+    ! compiler. We can't ensure that the two REAL kinds are actually
+    ! four and eight bytes long, but we can ensure that they are
+    ! distinct.
+
+    ! Local:
+    integer (kind = OneByteInt) :: One
+    integer (kind = TwoByteInt) :: Two
+    integer (kind = FourByteInt) :: Four
     integer (kind = EightByteInt) :: Eight
 
-    if (bit_size( One) == 8  .and. bit_size( Two)   == 16 .and.  &
-         bit_size(Four) == 32 .and. bit_size( Eight) == 64 .and.  &
-         FourByteReal > 0 .and. EightByteReal > 0 .and. &
-         FourByteReal /= EightByteReal) then
+    !-----------------------------------------------------------------------
+
+    if (bit_size( One) == 8 .and. bit_size( Two) == 16 &
+         .and. bit_size(Four) == 32 .and. bit_size( Eight) == 64 &
+         .and. FourByteReal > 0 .and. EightByteReal > 0 &
+         .and. FourByteReal /= EightByteReal) then
        byteSizesOK = .true.
     else
        byteSizesOK = .false.
     end if
+
   end function byteSizesOK
+
 end module type_sizes
