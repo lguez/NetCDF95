7e73a7af5dd167d13a241b2dffac761195209ef5
Lionel GUEZ <guez@lmd.ens.fr>
2023-07-26 09:36:26 +0200

    Add constants

--- nf95_constants.f90
+++ nf95_constants.f90
@@ -1,6 +1,7 @@
 module nf95_constants
 
   use nc_constants, only: NC_NOERR, NC_ENOGRP
+  use type_sizes, only: EightByteReal, TwoByteInt
 
   implicit none
 
@@ -10,19 +11,24 @@ module nf95_constants
   integer, parameter:: nf95_unlimited = 0
 
   ! External netcdf data types:
-  integer, parameter:: nf95_float = 5, nf95_double = 6
+  integer, parameter:: nf95_float = 5, nf95_double = 6, nf95_char = 2
 
   ! Mode flags for opening and creating a netcdf dataset:
   integer, parameter:: nf95_nowrite = 0, nf95_clobber = 0
-  integer, parameter:: nf95_write = 1
+  integer, parameter:: nf95_write = 1, nf95_noclobber = 4
 
   ! Implementation limits:
   integer, parameter:: nf95_max_name = 256
+  integer, parameter:: nf95_max_var_dims = 1024
 
   ! Default fill values:
   integer, parameter:: nf95_fill_int = - 2147483647
+  integer(kind =  TwoByteInt), parameter:: nf95_fill_short = -32767
   real, parameter:: nf95_fill_real = 9.9692099683868690e+36
+  real(kind = EightByteReal), parameter:: nf95_fill_double &
+       = 9.9692099683868690e36
+  character, parameter:: nf95_fill_char = achar(0)
 
-  private NC_NOERR, NC_ENOGRP
+  private NC_NOERR, NC_ENOGRP, EightByteReal, TwoByteInt
 
 end module nf95_constants
