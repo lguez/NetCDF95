7ca505050949e6dbbb36294b42e30c80e9800619
Lionel GUEZ <guez@lmd.ipsl.fr>
2024-10-25 13:11:00 +0200

    Overload with `nf95_get_att_one_oneByteInt`
    Overload `nf95_get_att` with `nf95_get_att_one_oneByteInt`.

--- Attributes/nf95_get_att.f90
+++ Attributes/nf95_get_att.f90
@@ -9,7 +9,7 @@ module nf95_get_att_m
   interface nf95_get_att
      module procedure nf95_get_att_text, nf95_get_att_one_twoByteInt, &
           nf95_get_att_one_FourByteInt, nf95_get_att_one_FourByteReal, &
-          nf95_get_att_one_eightByteReal
+          nf95_get_att_one_eightByteReal, nf95_get_att_one_oneByteInt
 
      ! The difference between the specific procedures is the type of
      ! argument "values".
@@ -125,6 +125,49 @@ contains
     end if
 
   end subroutine nf95_get_att_one_TwoByteInt
+
+  !***********************
+
+  subroutine nf95_get_att_one_oneByteInt(ncid, varid, name, values, ncerr)
+
+    use netcdf, only: nf90_get_att
+    use type_sizes, only: oneByteInt
+
+    integer, intent(in):: ncid, varid
+    character(len = *), intent(in):: name
+    integer (kind = oneByteInt), intent(out):: values
+    integer, intent(out), optional:: ncerr
+
+    ! Variables local to the procedure:
+    integer ncerr_not_opt
+    integer att_len
+
+    !-------------------
+
+    ! Check that the attribute contains a single value:
+    call nf95_inquire_attribute(ncid, varid, name, nclen=att_len, &
+         ncerr=ncerr_not_opt)
+    if (ncerr_not_opt == nf95_noerr) then
+       if (att_len /= 1) then
+          print *, "nf95_get_att_one_oneByteInt"
+          print *, "varid = ", varid
+          print *, "attribute name: ", name
+          print *, 'the attribute does not contain a single value'
+          print *, "number of values in attribute: ", att_len
+          stop 1
+       end if
+    end if
+
+    ncerr_not_opt = nf90_get_att(ncid, varid, name, values)
+    if (present(ncerr)) then
+       ncerr = ncerr_not_opt
+    else
+       if (ncerr_not_opt /= nf95_noerr) call &
+            nf95_abort("nf95_get_att_one_oneByteInt " // trim(name), &
+            ncerr_not_opt, ncid, varid)
+    end if
+
+  end subroutine nf95_get_att_one_oneByteInt
 
   !***********************
 
