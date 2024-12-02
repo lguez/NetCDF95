585eabcab8f291a80ed8f348f16dada38b6b873b
Lionel GUEZ <guez@lmd.ipsl.fr>
2024-12-02 16:23:13 +0100

    Check the type of argument `missing`
    Print a warning if the type of argument `missing` is not the type of
    the NetCDF attribute.

--- Attributes/nf95_get_missing.f90
+++ Attributes/nf95_get_missing.f90
@@ -5,6 +5,7 @@ module nf95_get_missing_m
   use nf95_abort_m, only: nf95_abort
   use nf95_constants, only: nf95_noerr
   use nf95_get_att_m, only: nf95_get_att
+  use nf95_inquire_attribute_m, only: nf95_inquire_attribute
 
   implicit none
 
@@ -21,13 +22,14 @@ contains
 
   subroutine nf95_get_missing_real(ncid, varid, missing)
 
-    use nf95_constants, only: nf95_fill_type => NF95_FILL_REAL
+    use nf95_constants, only: nf95_fill_type => NF95_FILL_REAL, &
+         nf95_type => nf95_float
 
     integer, intent(in)::  ncid, varid
     real, intent(out):: missing ! missing or fill value
 
     ! Local:
-    integer ncerr_1, ncerr_2
+    integer ncerr_1, ncerr_2, xtype
     real missing_1
 
     !-------------------------------------------------------------------
@@ -40,13 +42,14 @@ contains
 
   subroutine nf95_get_missing_dble(ncid, varid, missing)
 
-    use nf95_constants, only: nf95_fill_type => NF95_FILL_double
+    use nf95_constants, only: nf95_fill_type => NF95_FILL_double, &
+         nf95_type => nf95_double
 
     integer, intent(in)::  ncid, varid
     double precision, intent(out):: missing ! missing or fill value
 
     ! Local:
-    integer ncerr_1, ncerr_2
+    integer ncerr_1, ncerr_2, xtype
     double precision missing_1
 
     !-------------------------------------------------------------------
@@ -59,14 +62,15 @@ contains
 
   subroutine nf95_get_missing_short_int(ncid, varid, missing)
 
-    use nf95_constants, only: nf95_fill_type => NF95_FILL_short
+    use nf95_constants, only: nf95_fill_type => NF95_FILL_short, &
+         nf95_type => nf95_short
     use type_sizes, only: TwoByteInt
 
     integer, intent(in)::  ncid, varid
     integer(kind = TwoByteInt), intent(out):: missing ! missing or fill value
 
     ! Local:
-    integer ncerr_1, ncerr_2
+    integer ncerr_1, ncerr_2, xtype
     integer(kind = TwoByteInt) missing_1
 
     !-------------------------------------------------------------------
@@ -79,13 +83,14 @@ contains
 
   subroutine nf95_get_missing_int(ncid, varid, missing)
 
-    use nf95_constants, only: nf95_fill_type => NF95_FILL_INT
+    use nf95_constants, only: nf95_fill_type => NF95_FILL_INT, &
+         nf95_type => nf95_int
 
     integer, intent(in)::  ncid, varid
     integer, intent(out):: missing ! missing or fill value
 
     ! Local:
-    integer ncerr_1, ncerr_2
+    integer ncerr_1, ncerr_2, xtype
     integer missing_1
 
     !-------------------------------------------------------------------
@@ -98,7 +103,8 @@ contains
 
   subroutine nf95_get_missing_char(ncid, varid, missing)
 
-    use nf95_constants, only: nf95_fill_type => NF95_FILL_char
+    use nf95_constants, only: nf95_fill_type => NF95_FILL_char, &
+         nf95_type => nf95_char
 
     integer, intent(in)::  ncid, varid
 
@@ -107,7 +113,7 @@ contains
     ! a NetCDF character variable should be a single character
 
     ! Local:
-    integer ncerr_1, ncerr_2
+    integer ncerr_1, ncerr_2, xtype
     character missing_1
 
     !-------------------------------------------------------------------
@@ -120,14 +126,15 @@ contains
 
   subroutine nf95_get_missing_byte_int(ncid, varid, missing)
 
-    use nf95_constants, only: nf95_fill_type => NF95_FILL_byte
+    use nf95_constants, only: nf95_fill_type => NF95_FILL_byte, &
+         nf95_type => nf95_byte
     use type_sizes, only: oneByteInt
 
     integer, intent(in)::  ncid, varid
     integer(kind = oneByteInt), intent(out):: missing ! missing or fill value
 
     ! Local:
-    integer ncerr_1, ncerr_2
+    integer ncerr_1, ncerr_2, xtype
     integer(kind = oneByteInt) missing_1
 
     !-------------------------------------------------------------------
--- Attributes/nf95_get_missing.h
+++ Attributes/nf95_get_missing.h
@@ -2,10 +2,39 @@
   
   ! Body of specific procedures of generic interface "nf95_get_missing".
   
-  call nf95_get_att(ncid, varid, name = "missing_value", values = missing_1, &
-       ncerr = ncerr_1)
-  call nf95_get_att(ncid, varid, name = "_FillValue", values = missing, &
+  call nf95_inquire_attribute(ncid, varid, name = "missing_value", &
+       xtype = xtype, ncerr = ncerr_1)
+
+  if (ncerr_1 == nf95_noerr) then
+     call nf95_get_att(ncid, varid, name = "missing_value", values = missing_1)
+
+     if (xtype /= nf95_type) then
+        write(error_unit, fmt = *) "Warning: nf95_get_missing: type mismatch"
+        write(error_unit, fmt = *) "type of `missing` argument: ", nf95_type
+        write(error_unit, fmt = *) "type of NetCDF attribute missing_value: ", &
+             xtype
+        write(error_unit, fmt = *) "Converted missing_value:", missing_1
+        write(error_unit, fmt = *) "varid = ", varid
+        write(error_unit, fmt = *) "End of warning"
+     end if
+  end if
+
+  call nf95_inquire_attribute(ncid, varid, name = "_FillValue", xtype = xtype, &
        ncerr = ncerr_2)
+
+  if (ncerr_2 == nf95_noerr) then
+     call nf95_get_att(ncid, varid, name = "_FillValue", values = missing)
+
+     if (xtype /= nf95_type) then
+        write(error_unit, fmt = *) "Warning: nf95_get_missing: type mismatch"
+        write(error_unit, fmt = *) "type of `missing` argument: ", nf95_type
+        write(error_unit, fmt = *) "type of NetCDF attribute _FillValue: ", &
+             xtype
+        write(error_unit, fmt = *) "Converted _FillValue:", missing
+        write(error_unit, fmt = *) "varid = ", varid
+        write(error_unit, fmt = *) "End of warning"
+     end if
+  end if
 
   if (ncerr_1 == nf95_noerr) then
      if (ncerr_2 == nf95_noerr) then
