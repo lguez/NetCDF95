3848e01f7a651bd985428dbb1517404878b87c4e
Lionel GUEZ <guez@lmd.ipsl.fr>
2025-01-21 19:34:08 +0100

    Create program `test_gunp_var`

---
+++ Tests/test_gunp_var.f90
@@ -0,0 +1,20 @@
+program test_gunp_var
+
+  use netcdf95, only: nf95_gunp_var, nf95_open, nf95_close, nf95_nowrite, &
+       nf95_inq_varid
+
+  implicit none
+
+  integer ncid, varid
+  real adt(5)
+
+  !-------------------------------------------------------------------------
+
+  call nf95_open("adt.nc", nf95_nowrite, ncid)
+  call nf95_inq_varid(ncid, "adt", varid)
+  call nf95_gunp_var(ncid, varid, adt, start = [271, 1, 1], &
+       count_nc = [5, 1, 1])
+  print *, "adt = ", adt
+  call nf95_close(ncid)
+  
+end program test_gunp_var
