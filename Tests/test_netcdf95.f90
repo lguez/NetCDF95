591aed2728752e12da22236bfd77bf793a3480a7
Lionel GUEZ <guez@lmd.ipsl.fr>
2025-01-23 17:40:49 +0100

    Call `nf95_inq_libvers` in `test_netcdf95`
    Call `nf95_inq_libvers` instead of `nf90_inq_libvers` in program
    `test_netcdf95`.

--- Tests/test_netcdf95.f90
+++ Tests/test_netcdf95.f90
@@ -1,9 +1,6 @@
 program test_netcdf95
 
-  ! This is a program in Fortran 95.
-
-  use netcdf, only: nf90_inq_libvers
-  use netcdf95, only: nf95_open, nf95_close, nf95_nowrite
+  use netcdf95, only: nf95_open, nf95_close, nf95_nowrite, nf95_inq_libvers
 
   IMPLICIT none
 
@@ -11,7 +8,7 @@ program test_netcdf95
 
   !-----------------------------------------------------------------------
 
-  print *, "NetCDF-C library version: ", trim(nf90_inq_libvers())
+  call nf95_inq_libvers
   call nf95_open("plouf.nc", nf95_nowrite, ncid)
   call nf95_close(ncid)
 
