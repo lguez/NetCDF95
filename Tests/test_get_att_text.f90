4a91ea33b86174027f70bdd1f9cb5500a1fed036
Lionel GUEZ <guez@lmd.ipsl.fr>
2025-06-24 22:25:37 +0200

    Add the option to call with argument ncerr
    We want to test the call to `nf95_get_att` with and without this
    argument.

--- Tests/test_get_att_text.f90
+++ Tests/test_get_att_text.f90
@@ -1,15 +1,17 @@
 program test_get_att_text
 
   use jumble, only: get_command_arg_dyn
-  use netcdf95, only: nf95_get_att, nf95_open, nf95_nowrite, nf95_close
+  use netcdf95, only: nf95_get_att, nf95_open, nf95_nowrite, nf95_close, &
+       nf95_noerr, nf95_strerror
 
   implicit none
 
-  integer ncid
+  integer ncid, ncerr
+  logical:: recovery = .true.
   integer:: varid = 0, len_values = 65
   character(len = :), allocatable:: values, fname, att_name
   character(len = *), parameter:: usage = "test_get_att_text FILE-NAME ATT-NAME"
-  namelist /main_nml/ VARID, len_values
+  namelist /main_nml/ VARID, len_values, recovery
 
   !--------------------------------------------------------------------
 
@@ -19,8 +21,21 @@ program test_get_att_text
   read(unit = *, nml = main_nml)
   call nf95_open(fname, nf95_nowrite, ncid)
   allocate(character(len = len_values):: values)
-  call nf95_get_att(ncid, varid, att_name, values)
-  print *, 'values: "', values, '"'
+
+  if (recovery) then
+     call nf95_get_att(ncid, varid, att_name, values, ncerr)
+
+     if (ncerr == nf95_noerr) then
+        print *, 'values: "', values, '"'
+     else
+        print *, "ncerr =", ncerr
+        print *, nf95_strerror(ncerr)
+     end if
+  else
+     call nf95_get_att(ncid, varid, att_name, values)
+     print *, 'values: "', values, '"'
+  end if
+
   call nf95_close(ncid)
 
 end program test_get_att_text
