c2e7851d66e28f3c77491e195ec9931cf7ccd304
Lionel GUEZ <guez@lmd.ipsl.fr>
2025-06-24 22:21:06 +0200

    Generalize program `test_get_att_text`
    Read file name, varid, length of actual argument `values` and
    attribute name at run-time. This is useful to do several tests.

--- Tests/test_get_att_text.f90
+++ Tests/test_get_att_text.f90
@@ -1,17 +1,25 @@
 program test_get_att_text
 
-  use netcdf95, only: nf95_get_att, nf95_open, nf95_nowrite, nf95_close, &
-       NF95_GLOBAL
+  use jumble, only: get_command_arg_dyn
+  use netcdf95, only: nf95_get_att, nf95_open, nf95_nowrite, nf95_close
 
   implicit none
 
   integer ncid
-  character(len = 65) values
+  integer:: varid = 0, len_values = 65
+  character(len = :), allocatable:: values, fname, att_name
+  character(len = *), parameter:: usage = "test_get_att_text FILE-NAME ATT-NAME"
+  namelist /main_nml/ VARID, len_values
 
   !--------------------------------------------------------------------
 
-  call nf95_open("histhf.nc", nf95_nowrite, ncid)
-  call nf95_get_att(ncid, NF95_GLOBAL, name = "CDO", values= values)
+  call get_command_arg_dyn(1, fname, usage)
+  call get_command_arg_dyn(2, att_name, usage)
+  print *, "Enter main_nml:"
+  read(unit = *, nml = main_nml)
+  call nf95_open(fname, nf95_nowrite, ncid)
+  allocate(character(len = len_values):: values)
+  call nf95_get_att(ncid, varid, att_name, values)
   print *, 'values: "', values, '"'
   call nf95_close(ncid)
 
