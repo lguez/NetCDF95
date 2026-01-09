5f9b538aedd840e7b7377aa4991f8d2f5098d278
Lionel GUEZ <guez@lmd.ipsl.fr>
2026-01-09 23:27:10 +0100

    Generalize `test_find_coord`

--- Tests/test_find_coord.f90
+++ Tests/test_find_coord.f90
@@ -7,20 +7,26 @@ program test_find_coord
 
   integer ncid, dimid, varid, ncerr
   character(len = NF95_MAX_NAME) name
+  character(len = 9):: std_name = ""
+  logical:: recovery = .false.
+  namelist /main_nml/ std_name, recovery
 
   !------------------------------------------------------------------------
 
+  print *, "Enter namelist main_nml:"
+  read(unit = *, nml = main_nml)
   call nf95_open("adt.nc", nf95_nowrite, ncid)
-  call nf95_find_coord(ncid, name, dimid, varid, std_name = "longitude")
+
+  if (recovery) then
+     call nf95_find_coord(ncid, name, dimid, varid, std_name, ncerr)
+     print *, "ncerr = ", ncerr
+  else
+     call nf95_find_coord(ncid, name, dimid, varid, std_name)
+  end if
+
   print *, "name = ", trim(name)
   print *, "dimid = ", dimid
   print *, "varid = ", varid
-  call nf95_find_coord(ncid, name, dimid, varid, std_name = "plev", &
-       ncerr = ncerr)
-  print *, "name = ", trim(name)
-  print *, "dimid = ", dimid
-  print *, "varid = ", varid
-  print *, "ncerr = ", ncerr
   call nf95_close(ncid)
 
 end program test_find_coord
